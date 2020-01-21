cb156dc7d4d03df29578a3e6a1d792f48012e185
Muhammadamin Sarvaiya <muhammadamin.sarvaiya@citiustech.com>
2020-01-21 17:14:05 +0530

    comparing with date string function

--- lib/controllers/user_audit_report.controller.ts
+++ lib/controllers/user_audit_report.controller.ts
@@ -93,8 +93,8 @@ export class UserAuditReportController{
 				return res.status(200).send({
 					...{ userAuditReport },
 					...{
-						disableForm: !(startOfToday().toISOString() === startOfDay(new Date(req.body.reportingDate)).toISOString()),
-						serverDate: startOfToday().toISOString()
+						disableForm: !(startOfToday().toDateString() === startOfDay(new Date(req.body.reportingDate)).toDateString()),
+						serverDate: startOfToday().toDateString()
 					}
 				});
 			})
