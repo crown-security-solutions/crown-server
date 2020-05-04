0bf2cc81b37d9bd732fdb91767a6ba2b46723397
Muhammadamin Sarvaiya <muhammadamin.sarvaiya@citiustech.com>
2020-05-04 13:52:01 +0530

    new database added

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
 export const database = new Sequelize({
-  username: "KjFPObnKzN",
-	password: "KjFboFQ04Q",
-	database: "KjFPObnKzN",
+  username: "IKD3y9ntxb",
+	password: "h9u21TC9JK",
+	database: "IKD3y9ntxb",
 	host: "remotemysql.com",
 	port: 3306,
 	dialect: "mysql"
