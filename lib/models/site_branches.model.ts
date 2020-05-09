92f69f02f1f29d6799e1623830aa4938c1439f32
Muhammadamin Sarvaiya <muhammadamin.sarvaiya@citiustech.com>
2020-05-09 18:47:18 +0530

    created site_branches model and changed sites model

--- lib/models/site_branches.model.ts
+++ lib/models/site_branches.model.ts
@@ -2,16 +2,19 @@ import { Sequelize, Model, DataTypes, BuildOptions } from "sequelize";
 import { database } from "../config/database";
 import { SiteStrength } from "./site_strength.model";
 
-export class Site extends Model {
+export class SiteBranches extends Model {
   public id!: number;
 	public name: string;
+	public address: string;
+	public contact: string;
+	public contact_person: string;
 	public shift: number;
 	public active: boolean;
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 }
 
-Site.init(
+SiteBranches.init(
   {
 		id: {
       type: DataTypes.INTEGER.UNSIGNED,
@@ -19,19 +22,24 @@ Site.init(
       primaryKey: true
 		},
 		name: DataTypes.STRING,
-		shift: DataTypes.INTEGER,
+		code: DataTypes.STRING,
+		location: DataTypes.STRING,
+		zone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    contact: DataTypes.STRING,
+		contact_person: DataTypes.STRING,
     active: DataTypes.BOOLEAN
   },
   {
-    tableName: "sites",
+    tableName: "site_branches",
     sequelize: database // this bit is important
   }
 );
 
-Site.hasMany(SiteStrength, {
+SiteBranches.hasMany(SiteStrength, {
 	foreignKey: 'site_id',
 	sourceKey: 'id',
-	as: 'site_strengths'
+	as: 'sites_with_branches'
 });
 
 // Site.sync({ force: true }).then(() => console.log("Site table created"));
